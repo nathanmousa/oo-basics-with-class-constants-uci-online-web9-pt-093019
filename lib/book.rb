ddf613671473c338158df2a836c8b2a0f280bd35
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-21 22:49:21 +0000

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,9 +1,16 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
-
   def initialize(title)
     @title = title
+  end
+  
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
+
+  GENRES = []
+  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
   end
 
   def turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,19 @@
 class Shoe
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand if BRANDS.include?(brand) == false
+  end
+  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  def initialize(brand)
+  BRANDS = []
+  
+  def brand=(brand)
     @brand = brand
+    BRANDS << brand if BRANDS.include?(brand) == false
   end
-
+  
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
