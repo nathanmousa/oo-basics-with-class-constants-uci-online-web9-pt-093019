3a31bb8821f08b2ec6eed677733cdf352afba7eb
Nathan Mousa <nathanmousa1@gmail.com>
2019-10-31 01:11:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,17 +1,17 @@
 class Shoe
-  def initialize(brand)
-    @brand = brand
-    BRANDS << brand if BRANDS.include?(brand) == false
-  end
-  
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
-
+  
   BRANDS = []
+  
+  def initialize(brand)
+    @brand = brand
+    BRANDS << brand if !BRANDS.include?(brand)
+  end
   
   def brand=(brand)
     @brand = brand
-    BRANDS << brand if BRANDS.include?(brand) == false
+    BRANDS << brand if !BRANDS.include?(brand)
   end
   
   def cobble
